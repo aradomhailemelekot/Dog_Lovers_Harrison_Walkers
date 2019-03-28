5156d7315a3a941fa1d970cfb0622293ee0b5a1d
Your Name <you@example.com>
2019-03-28 12:03:22 -0400

    updated format

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Home from './Home.js';
-import logo from './logo.svg';
+import Logo from './color_logo_transparent.png';
 import Header from './Header.js';
+import './bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Home />
-        <Header />
+      <div className="container">
+        <div className="row">
+          <header className="col-sm-4 offset-sm-4">
+            <img src={Logo} className="mx-auto" alt="Logo" />
+          </header>
+        </div>
+        <div className="row">
+          <Home />
+          <Header />
+        </div>
       </div>
     );
   }
